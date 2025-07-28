4b53d9ca77438d930cd35f449c52cd204d4706e7
Balaji K <balaji181103@gmail.com>
2025-07-28 20:28:30 +0000

    balaji@kupparajs-MacBook-Air ~ % ifconfig | grep "inet " 	inet 127.0.0.1

--- uno_sim800l_http_test.ino
+++ uno_sim800l_http_test.ino
@@ -1,21 +1,17 @@
 #include <SoftwareSerial.h>
 
 // --- IMPORTANT: CONFIGURE THESE ---
-// APN (Access Point Name) for your SIM card's mobile network.
-// This is the gateway your SIM uses to connect to the internet.
-// "airtelgprs.com" is the standard APN for Airtel in India.
-const char* APN = "airtelgprs.com";
+// The APN (Access Point Name) for your SIM card's mobile carrier.
+// "airtelgprs.com" is a common one for Airtel in India.
+const char* APN = "airtelgprs.com"; 
 
-// Your computer's local IP address. The Arduino will send the HTTP request here.
-// Find this by running 'ipconfig' (on Windows) or 'ifconfig' (on Mac/Linux) in your terminal.
-// It will look something like "192.168.1.5" or "10.0.0.10".
-// DO NOT use "localhost" or "127.0.0.1".
-const char* SERVER_IP = "YOUR_COMPUTER_IP";
+// The local IP address of the computer running your Next.js server.
+const char* SERVER_IP = "192.168.1.35"; // Updated with your Mac's IP address.
 
 
-// --- Pin Definitions ---
-SoftwareSerial sim(10, 11);  // SIM800L TX -> Arduino 10, SIM800L RX -> Arduino 11
-
+// -- Hardware Pins --
+SoftwareSerial sim(10, 11);  // SIM800L TX -> 10, RX <- 11
+String number = "+918073667200"; // Change to your own number for SMS/Call tests
 
 void setup() {
   Serial.begin(9600);
@@ -23,83 +19,110 @@ void setup() {
   delay(1000);
 
   Serial.println("System Initialized.");
-  Serial.println("Type 'g' to perform an HTTP GET request to the server.");
+  Serial.println("Type 's' to send SMS, 'r' to receive SMS, 'c' to call, or 'g' to get customer data.");
 }
 
 void loop() {
-  // If the user sends a command from the Serial Monitor
   if (Serial.available()) {
     char cmd = Serial.read();
-    if (cmd == 'g') {
-      getCustomerDetails();
-    }
+    if (cmd == 's') SendMessage();
+    else if (cmd == 'r') ReceiveMessage();
+    else if (cmd == 'c') callNumber();
+    else if (cmd == 'g') getCustomerDetails();
   }
 
-  // Forward any messages from the SIM800L to the Serial Monitor
+  // Forward messages from SIM800L to Serial Monitor
   while (sim.available()) {
     Serial.write(sim.read());
   }
 }
 
+// Function to perform HTTP GET request
 void getCustomerDetails() {
-  if (SERVER_IP == "YOUR_COMPUTER_IP") {
-    Serial.println("ERROR: Please configure your computer's IP address in the SERVER_IP variable first!");
-    return;
-  }
+  Serial.println("Initializing GPRS...");
+  sim.println("AT+SAPBR=3,1,\"APN\",\"" + String(APN) + "\"");
+  delay(2000);
+  printResponse();
+
+  sim.println("AT+SAPBR=1,1"); // Enable GPRS
+  delay(3000);
+  printResponse();
+
+  Serial.println("Initializing HTTP...");
+  sim.println("AT+HTTPINIT");
+  delay(2000);
+  printResponse();
+
+  String url = "http://" + String(SERVER_IP) + ":9002/api/getMission?droneId=SB-001";
+  Serial.println("Setting URL: " + url);
+  sim.println("AT+HTTPPARA=\"URL\",\"" + url + "\"");
+  delay(2000);
+  printResponse();
+
+  Serial.println("Performing GET request...");
+  sim.println("AT+HTTPACTION=0"); // 0 for GET
+  delay(5000); // Wait for the action to complete
+  printResponse(); 
+
+  Serial.println("Reading response...");
+  sim.println("AT+HTTPREAD");
+  delay(3000);
+  printResponse();
+
+  Serial.println("Terminating HTTP...");
+  sim.println("AT+HTTPTERM");
+  delay(1000);
+  printResponse();
+
+  Serial.println("Disabling GPRS...");
+  sim.println("AT+SAPBR=0,1");
+  delay(2000);
+  printResponse();
   
-  Serial.println("Starting HTTP GET Request...");
-
-  // Sequence of AT commands for HTTP GET
-  // Each command is followed by a delay to allow the module to process it.
-
-  sendATCommand("AT", "OK", 1000);
-  sendATCommand("AT+SAPBR=3,1,\"Contype\",\"GPRS\"", "OK", 1000);
-  
-  String apnCmd = "AT+SAPBR=3,1,\"APN\",\"";
-  apnCmd += APN;
-  apnCmd += "\"";
-  sendATCommand(apnCmd, "OK", 1000);
-  
-  sendATCommand("AT+SAPBR=1,1", "OK", 2000); // Enable GPRS bearer
-  sendATCommand("AT+HTTPINIT", "OK", 2000);
-  sendATCommand("AT+HTTPPARA=\"CID\",1", "OK", 1000);
-
-  String urlCmd = "AT+HTTPPARA=\"URL\",\"http://";
-  urlCmd += SERVER_IP;
-  urlCmd += ":9002/api/getMission?droneId=SB-001\"";
-  sendATCommand(urlCmd, "OK", 1000);
-
-  sendATCommand("AT+HTTPACTION=0", "+HTTPACTION: 0,200", 10000); // Start GET, wait for 200 OK
-  sendATCommand("AT+HTTPREAD", "OK", 5000); // Read the response data
-  sendATCommand("AT+HTTPTERM", "OK", 1000); // Terminate HTTP session
-  sendATCommand("AT+SAPBR=0,1", "OK", 2000); // Disable GPRS bearer
-
-  Serial.println("HTTP GET Request Finished.");
+  Serial.println("--- Test Complete ---");
 }
 
-// Helper function to send an AT command and wait for a specific response
-void sendATCommand(String cmd, const char* expected_response, unsigned int timeout) {
-  String response = "";
-  
-  Serial.print("Sending: ");
-  Serial.println(cmd);
-  
-  sim.println(cmd);
-  
-  long int time = millis();
-  while ((time + timeout) > millis()) {
-    while (sim.available()) {
-      char c = sim.read();
-      response += c;
-    }
+// Helper function to print SIM800L responses to Serial Monitor
+void printResponse() {
+  while (sim.available()) {
+    Serial.write(sim.read());
   }
-  
-  Serial.print("Received: ");
-  Serial.println(response);
+}
 
-  if(response.indexOf(expected_response) != -1) {
-    Serial.println("Success");
-  } else {
-    Serial.println("Error");
-  }
+
+void SendMessage() {
+  Serial.println("Sending SMS...");
+
+  sim.println("AT+CMGF=1"); // Set text mode
+  delay(500);
+
+  sim.print("AT+CMGS=\"");
+  sim.print(number);
+  sim.println("\"");
+  delay(1000); // Wait for '>'
+
+  sim.print("Hello");
+  delay(500);
+  sim.write(26); // Send Ctrl+Z
+  delay(5000);
+
+  Serial.println("SMS Sent.");
+}
+
+void ReceiveMessage() {
+  Serial.println("Listening for incoming SMS...");
+  sim.println("AT+CMGF=1");        // Set text mode
+  delay(200);
+  sim.println("AT+CNMI=1,2,0,0,0"); // Show SMS on serial immediately
+  delay(1000);
+}
+
+void callNumber() {
+  Serial.println("Dialing...");
+  sim.print("ATD");
+  sim.print(number);
+  sim.println(";");
+  delay(10000); // Let it ring for 10 seconds
+  sim.println("ATH"); // Hang up
+  Serial.println("Call ended.");
 }
